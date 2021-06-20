d76509732672bdf0fc5a4e97b808571a5d14734d
scadaunity <scadaunity@gmail.com>
2021-06-20 11:35:21 -0300

    update index

--- public/index.php
+++ public/index.php
@@ -1,2 +1,10 @@
-<?php
-echo "Index.php";
+<!DOCTYPE html>
+<html lang="pt-br" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Scada Unity Framework</title>
+  </head>
+  <body>
+    <h1>Index.php</h1>
+  </body>
+</html>
