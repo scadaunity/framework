9d11d929d8b4c78e7b277bc88a88079939996c34
scadaunity <scadaunity@gmail.com>
2021-06-21 15:31:52 -0300

    create entry

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
     entry: './src/app.js',
     output: {
-        filename: 'js.js',
+        filename: 'app.js',
         path: path.resolve(__dirname, 'src/assets')
     },
     module:{
