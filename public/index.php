28bac9b0777e4dc7460766e6e23b58b33bca968b
scadaunity <scadaunity@gmail.com>
2021-06-21 21:12:41 -0300

    teste

--- public/index.php
+++ public/index.php
@@ -3,4 +3,4 @@
   //echo "<pre>";
   //var_dump($config);
 
-?> <!doctype html><html lang="pt-br" dir="ltr"><head><meta charset="utf-8"><title><?=$config['appTitle'] ?></title><script defer="defer" src="app.js"></script><script defer="defer" src="app.js"></script><script defer="defer" src="app.js"></script></head><body><h1 class="bg-red-900 text-white">Index.php</h1></body></html>
+?> <!doctype html><html lang="pt-br" dir="ltr"><head><meta charset="utf-8"><title><?=$config['appTitle'] ?></title><script defer="defer" src="app.js"></script><script defer="defer" src="app.js"></script><script defer="defer" src="app.js"></script><script defer="defer" src="app.js"></script></head><body><h1 class="bg-red-900 text-white">Index.php</h1></body></html>
