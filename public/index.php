c4789988560e492f366214cd4deb5b596e9fb0ee
scadaunity <scadaunity@gmail.com>
2021-06-25 21:49:06 -0300

    teste

--- public/index.php
+++ public/index.php
@@ -2,7 +2,7 @@
   require_once '../vendor/autoload.php';
 
   use App\Models\User;
-  use Scadaunity\Router;
+  use Scadaunity\Router\Router;
 
 
   var_dump(Router::get());
--- src/Router/Router.php
+++ src/Router/Router.php
@@ -7,11 +7,8 @@ namespace Scadaunity\Router;
  */
 class Router
 {
-  public $teste = 1;
-  function __construct(argument)
-  {
-    // code...
-  }
+
+  
 
   static function get(){
     return 'Ola mundo';
