4633fdf992e2c73552c522d7e9ca5ff8c6a77960
scadaunity <scadaunity@gmail.com>
2021-11-24 11:15:20 -0300

    create manisfest.php

---
+++ public/manifest.php
@@ -0,0 +1,20 @@
+{
+    "name": "Scada Unity",
+    "short_name": "ScadaUnity",
+    "description": "PHP - Framework",
+    "theme_color": "#000000",
+    "background_color": "#eeeeee",
+    "display": "fullscreen",
+    "scope": "/",
+    "start_url": "http://localhost:5000",
+    "orientation": "any",
+    "icons": [
+    {
+      "src": "assets/img/Logo.png",
+      "sizes": "173x173",
+      "type": "image\/png"
+    }
+
+  ]
+
+}
