839894541e3cdac49aaa0f5686ed3964241f1354
scadaunity <scadaunity@gmail.com>
2023-02-14 06:21:40 -0300

    update request

--- app/Controllers/AuthController.php
+++ app/Controllers/AuthController.php
@@ -2,11 +2,11 @@
 
 namespace App\Controllers;
 
-use ScadaUnity\Framework\Http\Request;
-use ScadaUnity\Framework\View\View;
-use ScadaUnity\Framework\Notification\Email;
-
 use App\Models\UserModel;
+use ScadaUnity\Framework\View\View;
+use ScadaUnity\Framework\Http\Request;
+
+use ScadaUnity\Framework\Notification\Email;
 
 /**
  * Classe responsavel pelo controle de acesso ao sistema
@@ -15,56 +15,60 @@ use App\Models\UserModel;
 class AuthController
 {
 
-    /**
-     * Metodo responsavel por retornar a tela de login
-     * @return string
-     */
-     public function login(){
-         $data = [
-             'title' => 'Login'
-         ];
-         view('template/header',$data);
-         view('components/Navbar',$data);
-         view('pages/auth/login',$data);
-         view('template/footer',$data);
+  /**
+   * Metodo responsavel por retornar a tela de login
+   * @return string
+   */
+  public function login()
+  {
+    $data = [
+      'title' => 'Login'
+    ];
+    view('template/header', $data);
+    view('components/Navbar', $data);
+    view('pages/auth/login', $data);
+    view('template/footer', $data);
   }
 
   /**
    * Metodo responsavel por retornar a tela de registro
    * @return string
    */
-  public function register(){
+  public function register()
+  {
 
     $data = [
       'title' => 'Criar conta'
     ];
 
-    view('template/header',$data);
+    view('template/header', $data);
     view('components/Navbar');
-    view('pages/auth/register',$data);
-    view('template/footer',$data);
+    view('pages/auth/register', $data);
+    view('template/footer', $data);
   }
 
   /**
    * Metodo responsavel por retornar a tela de recuperação da conta
    * @return string
    */
-  public function forgot(){
+  public function forgot()
+  {
 
     $data = [
       'title' => 'Recuperar conta'
     ];
 
-    view('template/header',$data);
-    view('pages/auth/forgot',$data);
-    view('template/footer',$data);
+    view('template/header', $data);
+    view('pages/auth/forgot', $data);
+    view('template/footer', $data);
   }
 
   /**
    * Metodo responsavel por enviar o e-mail de recuperação de senha
    * @return string
    */
-  public function sendResetPasswordByEmail(){
+  public function sendResetPasswordByEmail()
+  {
 
     // RECUPERA OS PARAMETROS DO POST
     $request = new Request();
@@ -81,44 +85,45 @@ class AuthController
     foreach ($users as $user) {
       /** VERIFICA SE EXISTE EMAIL CADASTRO */
       if ($user->email == $data['email']) {
-          $validUser = $user;
+        $validUser = $user;
       }
     }
 
     /** USUARIO NÃO ENCONTRADO RETORNA PRA LOGIN E EXIBE UMA MENSSAGEM*/
     if (!$validUser) {
-      setFlash('message','teste');
+      setFlash('message', 'teste');
       return redirect('/login');
     }
 
     $email = new Email();
 
     $email->add(
-        'Recuperação de senha',
-        '<h1>Olá, '.$validUser->name.'</h1><br><h3>Voce solicitou a recuperação da sua senha de acesso ao '.APP_TITLE.'</h3><br><br><a href="#">Recuperar minhs senha</a>',
-        $validUser->name,
-        $validUser->email
-    )->send(APP_TITLE,'scadaunity@gmail.com');
+      'Recuperação de senha',
+      '<h1>Olá, ' . $validUser->name . '</h1><br><h3>Voce solicitou a recuperação da sua senha de acesso ao ' . APP_TITLE . '</h3><br><br><a href="#">Recuperar minhs senha</a>',
+      $validUser->name,
+      $validUser->email
+    )->send(APP_TITLE, 'scadaunity@gmail.com');
 
-    if(!$email->error()){
+    if (!$email->error()) {
       redirect('/login');
-    }else{
+    } else {
       echo $email->error()->getMessage();
     }
-
   }
 
   /**
    * Metodo responsavel criar uma conta
    * @return view
    */
-  public function createAccount(){
+  public function createAccount()
+  {
 
     $validate = validate([
-      'name' => 'required',
       'email' => 'required|email|unique:users',
       'password' => 'required',
     ]);
+
+
 
     if (!$validate) {
       return redirect('/register');
@@ -131,7 +136,7 @@ class AuthController
     $data = [
       'name'      => $request->post()['name'],
       'email'     => $request->post()['email'],
-      'password'  => password_hash($request->post()['password'],PASSWORD_DEFAULT) 
+      'password'  => password_hash($request->post()['password'], PASSWORD_DEFAULT)
     ];
 
     $user = $userModel->save($data);
@@ -145,7 +150,8 @@ class AuthController
    * Metodo responsavel por destuir a seesão atual do usuario
    * @return  view
    */
-  public function destroy(){
+  public function destroy()
+  {
     unset($_SESSION[LOGGED]);
 
     redirect('/login');
@@ -155,42 +161,44 @@ class AuthController
    * Metodo responsavel por autentica o usuario
    * @return [type]
    */
-  public function autenticate(){
-  
-      $validate = validate([
-        'email' => 'required',
-        'password' => 'required',
-      ]);
+  public function autenticate()
+  {
 
-      if (!$validate) {
-        return redirect('/login');
-      }
+    // Valida a requisição
+    $validate = validate([
+      'email' => 'required',
+      'password' => 'required',
+    ]);
+
+    if (!$validate) {
+      return redirect('/login');
+    }
 
     /** ARMAZENA OS PARAMETROS DA REQUISIÇÃO*/
     $request = new Request();
-    $email = $request->post()['email'];
-    $password = $request->post()['password'];
+    $email = $request->email;
+    $password = $request->password;
 
     $errorMessage = 'Login invalido, por favor tente novamente';
 
     /** VERIFICA SE O EMAIL FOI PASSADO*/
     if (empty($email)) {
       $errorMessage = '*Campo email obrigatorio';
-      setFlash('email',$errorMessage);
+      setFlash('email', $errorMessage);
       return redirect('/login');
     }
 
     /** VERIFICA SE O PASSWORD FOI PASSADO*/
     if (empty($password)) {
       $errorMessage = '*Campo senha obrigatorio';
-      setFlash('password',$errorMessage);
+      setFlash('password', $errorMessage);
       return redirect('/login');
     }
 
     /** BUSCA OS USUARIOS NO BANCO */
     $model = new UserModel();
     $users = $model->all();
-    
+
 
     $validUser = null;
 
@@ -198,21 +206,21 @@ class AuthController
       /** VERIFICA SE EMAIL CONFEREM */
       if ($user->email == $email) {
         $validUser = $user;
-      }      
+      }
     }
 
     /** USUARIO NÃO ENCONTRADO RETORNA PRA LOGIN E EXIBE UMA MENSSAGEM*/
     if (!$validUser) {
-      setFlash('invalidLogin',$errorMessage);
+      setFlash('invalidLogin', $errorMessage);
       return redirect('/login');
     }
 
     // VERIFICA A SENHA
 
-    
 
-    if(!password_verify($password,$validUser->password)){
-      setFlash('invalidLogin',$errorMessage);
+
+    if (!password_verify($password, $validUser->password)) {
+      setFlash('invalidLogin', $errorMessage);
       return redirect('/login');
     }
 
@@ -220,15 +228,15 @@ class AuthController
     $_SESSION[LOGGED] = $validUser;
 
     /** REDIRECIONA PARA A HOME */
-     return redirect('/home');
-
+    return redirect('/home');
   }
 
   /**
    * Metodo responsavel por exibir os termos do serviço
    * @return view
    */
-  public function therms(){
+  public function therms()
+  {
     view('template/header');
     view('auth/therms');
     view('template/footer');
@@ -238,7 +246,8 @@ class AuthController
    * Metodo responsavel por exibir a politica de privacidade
    * @return view
    */
-  public function privacy(){
+  public function privacy()
+  {
     view('template/header');
     view('auth/privacy');
     view('template/footer');
